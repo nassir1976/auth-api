c4a7138c1febb58aff8fa16ba14d1364cb0eb8ff
nassir <daniya_n@yahoo.com>
2021-03-17 19:49:50 -0700

    lab-08

--- index.js
+++ index.js
@@ -7,14 +7,14 @@ const server = require('./src/server.js');
 // ==========connecting to our DB===========
 
 const PORT = process.env.PORT || 3333;
-const MONGODB_URI = 'mongodb://localhost:27017/api-server'; // api-server is the DB name
+// const MONGODB_URI = 'mongodb://localhost:27017/api-server'; // api-server is the DB name
 const options = { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true,}; // always pass in these options
 
 // mongoose.connect(MONGODB_URI, options);
 
 // server.start(PORT);
 
-mongoose.connect(MONGODB_URI, options)
+mongoose.connect(process.env.MONGODB_URI, options)
   .then(() => {
     server.start(PORT, () => console.log(`server up: ${PORT}`));
   })
